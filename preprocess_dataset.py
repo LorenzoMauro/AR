eda08f2a704da1650bcf16113270690e10b73438
LorenzoMauro <lorenzo.lomar.mauro@gmail.com>
2019-05-06 17:34:45 +0200

    adding h5

--- preprocess_dataset.py
+++ preprocess_dataset.py
@@ -10,7 +10,6 @@ import pickle
 from dataset_manager import Dataset
 import config
 import multiprocessing.dummy as mp
-from PIL import Image
 import datetime
 import tensorflow as tf
 import json
@@ -22,7 +21,7 @@ import time
 class preprocess:
     def __init__(self):
         with tf.Session() as sess:
-            with h5py.File("Data/processed/dataset.h5", "w") as hf:
+            with h5py.File("Dataset/preprocessed/dataset.h5", "w") as hf:
                 self.sess = sess
                 self.openpose = OpenPose(self.sess)
                 self.openpose.load_openpose_weights()
@@ -42,19 +41,25 @@ class preprocess:
                             #Zero Frame
                             video.set(1, 0)
                             ret, prev = video.read()
-                            dict_video[0] = {}
-                            dict_video[0]['rgb'] = prev
-                            dict_video[0]['gray'] = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
+                            print(ret)
+                            print(type(prev))
+                            gray =  cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
+                            dset = hf.create_dataset(path + '+' + str(0) + "+gray", data=gray)
+                            dset = hf.create_dataset(path + '+' + str(0) + "+rgb", data=prev)
+
                             pbar_frame = tqdm(total=length, leave=False, desc='Frame')
 
                             for frame in range(1, length):
+                                
                                 try:
+                                    base = path + '+' + str(frame)
                                     video.set(1, frame)
                                     ret, im = video.read()
                                     video.set(1, frame)
                                     ret, im = video.read()
                                     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-                                    gray_prev = dict_video[frame-1]['gray']
+                                    gray_prev = hf[path + '+' + str(frame-1) + '+gray']
+                                    gray_prev = np.array(gray_prev)
                                     flow = cv2.calcOpticalFlowFarneback(gray_prev, gray, flow=None,
                                                                     pyr_scale=0.5, levels=1,
                                                                     winsize=15, iterations=3,
@@ -63,28 +68,18 @@ class preprocess:
                                     norm_flow = cv2.normalize(flow, norm_flow, 0, 255, cv2.NORM_MINMAX)
                                     res_im = cv2.resize(im, dsize=(368, 368), interpolation=cv2.INTER_CUBIC)
                                     pafMat, heatMat = self.openpose.compute_pose_frame(res_im)
-                                    base = path + '_' + str(frame)
-                                    dset = hf.create_dataset(base + "_rgb", data=im)
-                                    dset = hf.create_dataset(base + "_gray", data=gray)
-                                    dset = hf.create_dataset(base + "_of", data=norm_flow)
-                                    dset = hf.create_dataset(base + "_pafmat", data=pafMat)
-                                    dset = hf.create_dataset(base + "_heatMat", data=heatMat)
-
-                                    # dict_video[frame] = {}
-                                    # dict_video[frame]['rgb'] = im
-                                    # dict_video[frame]['gray'] = gray
-                                    # dict_video[frame]['of'] = norm_flow
-                                    # dict_video[frame]['pafmat'] = pafMat
-                                    # dict_video[frame]['heatMat'] = heatMat
+                                    dset = hf.create_dataset(base + "+rgb", data=im)
+                                    dset = hf.create_dataset(base + "+gray", data=gray)
+                                    dset = hf.create_dataset(base + "+of", data=norm_flow)
+                                    dset = hf.create_dataset(base + "+pafmat", data=pafMat)
+                                    dset = hf.create_dataset(base + "+heatMat", data=heatMat)
                                 except Exception as e:
+                                    print(e)
                                     print(path + '    frame:' + str(frame))
                                     pass
                                 pbar_frame.update(1)
                             pbar_frame.refresh()
-                            # pbar_frame.clear()
                             pbar_frame.close()
-                            # with open(root + '/' + fl.split('.')[0] + '.pkl', 'wb') as f:
-                            #     pickle.dump(dict_video, f, pickle.HIGHEST_PROTOCOL)
 
                         pbar_file.update(1)
                 pbar_file.refresh()
